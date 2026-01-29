c5833922739b36358c4449bea4555103bb34daaa
paskaen <49122307+paskaen@users.noreply.github.com>
2026-01-30 01:23:41 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+# 安装  
+
+> **注意：** 记得把命令中的 `yourdomain` 改为你真实的域名
+
+```bash 
+curl -Ls https://raw.githubusercontent.com/frankiejun/node-ws/refs/heads/main/setup.sh > setup.sh && chmod +x setup.sh && ./setup.sh yourdomain
+```
+
+> webhostmost目前已禁用，webfreecloud上虽然没有终端但运行良好，另外忠告：不想封号的话别装探针。  
+
+### 查看节点  
+https://你的域名/你的uuid  
+
+### 查看进程  
+https://你的域名/ps
+
+### 执行shell命令  
+https://你的域名/你的uuid/cmd/你的命令 (空格用 $ 代替) 
+
+
+### 关于保活  
+默认自动保活哪吒和节点(webhostmost保活哪吒和一定程度保活节点)，无需你特殊处理。  
+
+
+
+# Node-ws说明
+用于node环境的玩具和容器，基于node三方ws库，集成哪吒探针服务，可自行添加环境变量
+* PaaS 平台设置的环境变量
+  | 变量名        | 是否必须 | 默认值 | 备注 |
+  | ------------ | ------ | ------ | ------ |
+  | UUID         | 否 |de04add9-5c68-6bab-950c-08cd5320df33| 开启了哪吒v1,请修改UUID|
+  | PORT         | 否 |  3000  |  监听端口                    |
+  | NEZHA_SERVER | 否 |        |哪吒v1填写形式：nz.abc.com:8008   哪吒v0填写形式：nz.abc.com|
+  | NEZHA_PORT   | 否 |        | 哪吒v1没有此变量，v0的agent端口| 
+  | NEZHA_KEY    | 否 |        | 哪吒v1的NZ_CLIENT_SECRET或v0的agent端口 |
+  | NAME         | 否 |        | 节点名称前缀，例如：Glitch |
+  | DOMAIN       | 是 |        | 项目分配的域名或已反代的域名，不包括https://前缀  |
+  | AUTO_ACCESS  | 否 |  true  | 是否开启自动访问保活,false为关闭,true为开启,需同时填写DOMAIN变量 |
+
+
+* js混肴地址：https://obfuscator.io  
+
---
+++ m_setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Error: 参数为你的域名！"
+    echo "Usage: $0 domain"
+    exit 1
+fi
+
+domain=$1
+username=$(whoami)
+random_port=$((RANDOM % 40001 + 20000))  
+
+
+read -p "输入UUID:" uuid
+if [ -z "$uuid" ]; then
+    echo "Error: UUID不能为空！"
+    exit 1
+fi
+echo "你输入的UUID: $uuid"
+read -p "是否安装探针? [y/n] [n]:" input
+input=${input:-n}
+if [ "$input" != "n" ]; then
+   read -p "输入NEZHA_SERVER(哪吒v1填写形式：nz.abc.com:8008,哪吒v0填写形式：nz.abc.com):" nezha_server
+   if [ -z "$nezha_server" ]; then
+    echo "Error: nezha_server不能为空！"
+    exit 1
+  fi
+  read -p "输入NEZHA_PORT( v1面板此处按回车, v0的agent端口为{443,8443,2096,2087,2083,2053}其中之一时开启tls):" nezha_port
+  nezha_port=${nezha_port:-""}
+  read -p "输入NEZHA_KEY(v1的NZ_CLIENT_SECRET或v0的agent端口):" nezha_key
+  if [ -z "$nezha_key" ]; then
+    echo "Error: nezha_key不能为空！"
+    exit 1
+  fi
+fi
+echo "你输入的nezha_server: $nezha_server, nezha_port:$nezha_port, nezha_key:$nezha_key"
+
+sed -i "s/NEZHA_SERVER || ''/NEZHA_SERVER || '$nezha_server'/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/NEZHA_PORT || ''/NEZHA_PORT || '$nezha_port'/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/NEZHA_KEY || ''/NEZHA_KEY || '$nezha_key'/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/#DOMAIN#/$domain/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/3000;/$random_port;/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s/#UUID#/$uuid/g" "/home/$username/domains/$domain/public_html/index.js"
+sed -i "s|/HOME|/home/$username|g" "/home/$username/domains/$domain/public_html/index.js"
+
+
+echo "安装完毕" 
